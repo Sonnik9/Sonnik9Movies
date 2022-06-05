2ebb6c9909729fddb2bb6198ff1edd73d3e0c29b
oleh-zuiev <64410055+oleh-zuiev@users.noreply.github.com>
2022-06-05 18:29:58 +0300

    adds routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { MoviesPageComponent } from './movies-page/movies-page.component';
+import { PeopleComponent } from './people/people.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { TvShowsComponent } from './tv-shows/tv-shows.component';
+const routes: Routes = [
+  { path: '', component: MoviesPageComponent },
+  { path: 'people', component: PeopleComponent },
+  { path: 'leaderboard', component: LeaderboardComponent },
+  { path: 'tvshows', component: TvShowsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesPageComponent } from './movies-page/movies-page.component';
 import { HeaderComponent } from './header/header.component';
+import { TvShowsComponent } from './tv-shows/tv-shows.component';
+import { PeopleComponent } from './people/people.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesPageComponent,
-    HeaderComponent
+    HeaderComponent,
+    TvShowsComponent,
+    PeopleComponent,
+    LeaderboardComponent
   ],
   imports: [
     BrowserModule,
