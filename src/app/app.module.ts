29eafa8195686ced56d0f2fee7049caa455398ab
Serhii Biriyk <bir3971@gmail.com>
2022-06-05 16:17:32 +0300

    do Html and css, without logick
    I do it my tasck without logick, becouse i cann`t do this. I need more knowlege.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesPageComponent } from './movies-page/movies-page.component';
+import { TopRatedMoviesComponent } from './top-rated-movies/top-rated-movies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesPageComponent
+    MoviesPageComponent,
+    TopRatedMoviesComponent
   ],
   imports: [
     BrowserModule,
