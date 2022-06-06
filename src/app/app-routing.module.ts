be268cbed7a5010f11b2355629530ef0a9e54139
oleh-zuiev <64410055+oleh-zuiev@users.noreply.github.com>
2022-06-06 23:18:52 +0300

    fonts in global scss file and default routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import { PeopleComponent } from './people/people.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { TvShowsComponent } from './tv-shows/tv-shows.component';
 const routes: Routes = [
-  { path: '', component: MoviesPageComponent },
+  { path: 'movies', component: MoviesPageComponent },
   { path: 'people', component: PeopleComponent },
   { path: 'leaderboard', component: LeaderboardComponent },
   { path: 'tvshows', component: TvShowsComponent },
+  { path: '', redirectTo: '/movies', pathMatch: 'full' },
+  { path: '**', redirectTo: '/movies' },
 ];
 
 @NgModule({
