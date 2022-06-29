ec88faa7830f7f985bfa03495dadcdd72d61d174
Andrii Suprun <andriisuprun@Andriis-MacBook-Air.local>
2022-06-29 17:15:05 +0300

    restructured project tree

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MoviesPageComponent } from './movies-page/movies-page.component';
-import { TopRatedMoviesComponent } from './movies-page/top-rated-movies/top-rated-movies.component';
-import { HeaderComponent } from './header/header.component';
-import { TvShowsComponent } from './tv-shows/tv-shows.component';
-import { PeopleComponent } from './people/people.component';
-import { LeaderboardComponent } from './leaderboard/leaderboard.component';
-import { ComingSoonComponent } from './movies-page/coming-soon/coming-soon.component';
-import { TrendingComponent } from './movies-page/trending/trending.component';
+import { MoviesPageComponent } from './components/movies-page/movies-page.component';
+import { TopRatedMoviesComponent } from './components/movies-page/top-rated-movies/top-rated-movies.component';
+import { HeaderComponent } from './core-components/header/header.component';
+import { TvShowsComponent } from './components/tv-shows/tv-shows.component';
+import { PeopleComponent } from './components/people/people.component';
+import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
+import { ComingSoonComponent } from './components/movies-page/coming-soon/coming-soon.component';
+import { TrendingComponent } from './components/movies-page/trending/trending.component';
 
 @NgModule({
   declarations: [
