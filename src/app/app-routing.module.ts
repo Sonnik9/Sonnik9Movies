e0eee763d3a4ed41d446bbb8cf2b67d18046e366
Andrii Suprun <andriisuprun@Andriis-MacBook-Air.local>
2022-06-29 17:16:32 +0300

    fixed imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MoviesPageComponent } from './movies-page/movies-page.component';
-import { PeopleComponent } from './people/people.component';
-import { LeaderboardComponent } from './leaderboard/leaderboard.component';
-import { TvShowsComponent } from './tv-shows/tv-shows.component';
+import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
+import { MoviesPageComponent } from './components/movies-page/movies-page.component';
+import { PeopleComponent } from './components/people/people.component';
+import { TvShowsComponent } from './components/tv-shows/tv-shows.component';
 
 const routes: Routes = [
   { path: 'movies', component: MoviesPageComponent },
