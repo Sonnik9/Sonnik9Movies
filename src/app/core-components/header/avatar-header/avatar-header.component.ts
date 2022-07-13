4fe88f49ab5405cb543e83c495d11435fb760c10
Sonnik9 <nikolas.smsttt@gmail.com>
2022-07-13 20:05:26 +0400

    sec precomit avatar

--- src/app/core-components/header/avatar-header/avatar-header.component.ts
+++ src/app/core-components/header/avatar-header/avatar-header.component.ts
@@ -15,52 +15,45 @@ export class AvatarHeaderComponent implements OnInit {
   public circleColor: any;
 
   private colors = [
-      '#EB7181', // red
-      '#468547', // green
-      '#FFD558', // yellow
-      '#3670B2', // blue
-      '#3700B3',
-      '#344955',
-      '#5D1049',
-      '#03DAC5',
-      '#F4511E',
-      '#009FD4',
+    '#EB7181', // red
+    '#468547', // green
+    '#FFD558', // yellow
+    '#3670B2', // blue
+    '#3700B3',
+    '#344955',
+    '#5D1049',
+    '#03DAC5',
+    '#F4511E',
+    '#009FD4',
   ];
 
   constructor() {}  
 
   ngOnInit() { 
-
-      if (!this.user.photoUrl) {
-          this.showInitials = true;
-          this.createInititals();
-          this.generateId(); 
-          this.circleColor = this.colors[this.idStr];
-      }
-
-      if (!this.user.photoUrl && !this.user.name) {
-        // this.circleColor = this.colors[0];
-        // this.initials = "A";
-        this.showInitials = false;
-        this.user.photoUrl = this.user.photoUrlDefolt;
-      }
+    if (!this.user.photoUrl) {
+      this.showInitials = true;
+      this.createInititals();
+      this.generateId(); 
+      this.circleColor = this.colors[this.idStr];
+    }
+    if (!this.user.photoUrl && !this.user.name) {
+      this.showInitials = false;
+      this.user.photoUrl = this.user.photoUrlDefolt;
+      this.circleColor = 'rgb(240, 240, 240)';
+    }
   }
 
   private createInititals(): void {
-    let initials = "";
-    initials = this.user.name.substr(0, 1);
-    this.initials = initials;     
+    this.initials = this.user.name.substr(0, 1);     
   }
 
-  private generateId() {  
-    let idStr = "";  
+  private generateId(): void {  
     let protoIdNum = 0;
     for (let i = 0; i < this.user.name.length; i++) {
       protoIdNum = protoIdNum + this.user.name.charCodeAt(i)   
     }
     protoIdNum = protoIdNum + 123456789;
-    idStr = String(protoIdNum).slice(-1);
-    this.idStr = idStr;      
+    this.idStr = String(protoIdNum).slice(-1);       
   }
 
 }
--- src/app/core-components/header/header.component.ts
+++ src/app/core-components/header/header.component.ts
@@ -7,11 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit { 
   user = {
-    // name:  "Irena",
-    name: "",
-    // photoUrl: "../../../assets/avatar-header.img/20210509_124052.jpg",
-    photoUrl: "",
-    photoUrlDefolt: "../../../assets/avatar-header.img/4213460-account-avatar-head-person-profile-user_115386.png",
+    name:  "Nik",
+    photoUrl: "",    
+    photoUrlDefolt: "../../../assets/header/login.svg",
     id: "",
   };  
 
